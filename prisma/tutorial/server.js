0454143547701aeb6a82f7d4b8f672dbc0d2d789
sasama <mametaku3@yahoo.co.jp>
2024-06-27 20:41:53 +0900

    Add: prismaチュートリアル用のAPIサーバをNode.jsとexpressで実装

--- prisma/tutorial/server.js
+++ prisma/tutorial/server.js
@@ -1,7 +1,29 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
-const PORT = 9000;
+const PORT = 8080;
+
+const prisma = new PrismaClient();
+
+// // expressでjsonのミドルウェアを使用（json形式のpostを受け取るため）
+app.use(express.json());
+
+// expressで/createに対するpostの受取処理
+app.post("/create", async(req, res) => {
+	// post値を取得{ todo }とすることで、jsonのtodoの値を取得できる
+	const { todo } = req.body;
+
+	// prisma.[Model名].[prisma組み込み関数]
+	// create()はprismaの組み込み関数
+	const posts = await prisma.todos.create({
+		data: {
+			todo: todo,
+			status: 0, // status:0 未完了
+		}
+	});
+	return res.json(posts);
+});
 
 app.listen(PORT, () => {
-	console.log("サーバーが起動中...");
+	console.log(`Server is running at http://localhost:${PORT}`);
 });
