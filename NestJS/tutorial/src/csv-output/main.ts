2b69d445ca3ab363a3aba1de5217c6768d1cf890
sasama <mametaku3@yahoo.co.jp>
2024-07-21 12:05:11 +0900

    Update: 今回はスクリプトの実行だけできれば良いので、開発サーバの軌道処理は削除

--- NestJS/tutorial/src/csv-output/main.ts
+++ NestJS/tutorial/src/csv-output/main.ts
@@ -4,8 +4,6 @@ import { CsvOutputService } from './csv-output.service';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-  // 3000番ポートでサーバ起動
-  await app.listen(3000);
 
   // 静的モジュールからプロバイダーのインスタンスを取得
   const csvOutputService = app.get(CsvOutputService);
