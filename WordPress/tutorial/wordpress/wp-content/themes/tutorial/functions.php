b6d3e8e5b5785f5c503eae01e7dafe305bcb1846
sasama <mametaku3@yahoo.co.jp>
2024-08-20 21:56:28 +0900

    Update: 引数で無名関数を渡すよう修正（関数名をつける必要もないため）

--- WordPress/tutorial/wordpress/wp-content/themes/tutorial/functions.php
+++ WordPress/tutorial/wordpress/wp-content/themes/tutorial/functions.php
@@ -2,9 +2,8 @@
 /**
  * サムネイルの有効化
  */
-add_action('init', 'enable_thumbnails');
-function enable_thumbnails() {
+add_action('init', function() {
   add_theme_support('post-thumbnails');
-}
+});
 
 ?>
