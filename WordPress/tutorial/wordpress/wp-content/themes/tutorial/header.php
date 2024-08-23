dc7fd6468c5da7d6ea786fff1eccf09a6a402b1a
sasama <mametaku3@yahoo.co.jp>
2024-08-23 22:48:32 +0900

    Update: head部分をパーツ化

--- WordPress/tutorial/wordpress/wp-content/themes/tutorial/header.php
+++ WordPress/tutorial/wordpress/wp-content/themes/tutorial/header.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>WordPressチュートリアル</title>
-  <meta name="description" content="WordPressチュートリアル">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?= get_stylesheet_uri(); ?>">
-  <?php wp_head(); ?>
-</head>
+<?= get_template_part('parts/head'); ?>
 <body>
   <header>
     <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
@@ -22,7 +15,7 @@
               <a class="nav-link" aria-current="page" href="/">トップ</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">投稿一覧</a>
+              <a class="nav-link" href="/tech">投稿一覧</a>
             </li>
           </ul>
         </div>
---
+++ WordPress/tutorial/wordpress/wp-content/themes/tutorial/parts/head.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <title>WordPressチュートリアル</title>
+  <meta name="description" content="WordPressチュートリアル">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= get_stylesheet_uri(); ?>">
+  <?php wp_head(); ?>
+</head>
