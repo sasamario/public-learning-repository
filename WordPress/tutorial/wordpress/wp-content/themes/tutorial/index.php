5ee1d52267f8b391dd4ff603dd869249d5ecfc3f
sasama <mametaku3@yahoo.co.jp>
2024-08-26 13:59:55 +0900

    Update: 文言追加

---
+++ WordPress/tutorial/wordpress/wp-content/themes/tutorial/index.php
@@ -0,0 +1,11 @@
+<?php
+  get_header();
+?>
+
+<div class="container my-5">
+  <p>アクセスしたページはURLが正しくないか、すでに削除されています。トップページに戻って再度確認してください。</p>
+  <a href="/">トップへ戻る</a>
+</div>
+
+<?php
+  get_footer();
