65e4a7b8b845b0ba506bdae7aee49a38e519b43f
sasama <mametaku3@yahoo.co.jp>
2024-09-08 16:52:07 +0900

    Update: ルートコンポーネントの修正、カードコンポーネントの追加

--- Vue.js/tutorial2/vue-tutorial/src/App.vue
+++ Vue.js/tutorial2/vue-tutorial/src/App.vue
@@ -1,47 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  // コンポーネントのローカル登録
+  components: {
+    Card,
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container my-5">
+    <div class="row my-3">
+      <div class="col">
+        <Card title="test" />
+      </div>
+      <div class="col">
+        <Card title="test2" />
+      </div>
+      <div class="col">
+        <Card title="test3" />
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
---
+++ Vue.js/tutorial2/vue-tutorial/src/components/Card.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">hogehoge</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
