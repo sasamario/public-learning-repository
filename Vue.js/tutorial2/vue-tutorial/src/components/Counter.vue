910b7130843030463a2b6a418986a145bc587c6b
sasama <mametaku3@yahoo.co.jp>
2024-09-08 19:44:50 +0900

    Add: Counterコンポーネントを追加

---
+++ Vue.js/tutorial2/vue-tutorial/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p class="text-center">{{ count }}</p>
+    <div class="row">
+      <button class="col mx-1 btn btn-primary" @click="increment()">
+        increment
+      </button>
+      <button class="col mx-1 btn btn-primary" @click="decrement()">
+        decrement
+      </button>
+    </div>
+  </div>
+</template>
