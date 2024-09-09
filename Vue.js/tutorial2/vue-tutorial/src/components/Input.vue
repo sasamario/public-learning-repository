fe430172bc85994822eaf88c4497540bd6f111ba
sasama <mametaku3@yahoo.co.jp>
2024-09-09 21:45:14 +0900

    Add: 双方向バインディングの簡単な例としてInputコンポーネントを追加

---
+++ Vue.js/tutorial2/vue-tutorial/src/components/Input.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p class="text-center">text: {{ text }}</p>
+    <div class="row">
+      <input v-model="text" maxlength="15" />
+    </div>
+  </div>
+</template>
