80436933b7b9a4e47478e1cceb84f0b8f47f7dca
sasama <mametaku3@yahoo.co.jp>
2024-09-10 21:42:46 +0900

    Add: Todoコンポーネントを追加

---
+++ Vue.js/tutorial2/vue-tutorial/src/components/Todo.vue
@@ -0,0 +1,47 @@
+<script>
+let id = 1;
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: [
+        {
+          id: id++,
+          text: "Todo1",
+        },
+      ],
+    };
+  },
+  methods: {
+    add(newTodo) {
+      this.todos.push({
+        id: id++,
+        text: newTodo,
+      });
+      this.newTodo = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="row">
+      <div class="col-8">
+        <input v-model="newTodo" maxlength="15" />
+      </div>
+      <div class="col-4">
+        <button type="button" class="btn btn-primary" @click="add(newTodo)">
+          追加
+        </button>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <ul class="list-group">
+        <li v-for="todo in todos" :key="todo.id" class="list-group-item">
+          {{ todo.id }}： {{ todo.text }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
