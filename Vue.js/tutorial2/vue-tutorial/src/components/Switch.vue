52cb035f91ecab809581fc7258ee98e750a8c128
sasama <mametaku3@yahoo.co.jp>
2024-09-12 21:51:36 +0900

    Add: v-showディレクティブの使用例としてスイッチコンポーネントを追加

---
+++ Vue.js/tutorial2/vue-tutorial/src/components/Switch.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="row form-check form-switch mx-4">
+      <input
+        v-model="isShow"
+        class="form-check-input"
+        type="checkbox"
+        role="switch"
+        id="flexSwitchCheckChecked"
+      />
+      <label
+        v-show="isShow"
+        class="form-check-label"
+        for="flexSwitchCheckChecked"
+        >^ - ^ 「Hello World」</label
+      >
+    </div>
+  </div>
+</template>
